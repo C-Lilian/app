1a0fcb8b42d17b75fce47491089de0a10d3a9f35
lucas-971 <lcantin971@gmail.com>
2024-06-27 12:02:16 +0200

    first commit

---
+++ app/assets/javascript/application.js
@@ -0,0 +1,7 @@
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
---
+++ app/javascript/application.js
@@ -0,0 +1,12 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+import "@hotwired/turbo-rails";
+import "controllers";
+import 'bootstrap';
